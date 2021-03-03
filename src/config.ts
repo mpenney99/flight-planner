c9a4e146e52eab4bee6a5f483feeb0a1b397ecc3
Michael Penney <michael.penney@unifly.aero>
2021-03-03 09:32:22 +0100

    cleanup

--- src/config.ts
+++
@@ -1,9 +0,0 @@
-export const MAPBOX_ACCESS_TOKEN =
-    'pk.eyJ1IjoibXBlbm5leTk5IiwiYSI6ImNrbG1paG40YzA5ZjUybnF5aGZobGFlMHcifQ.le-be8-cf0Y9FNt56KYqAw';
-
-export const API_KEY = '';
-export const USERNAME = '';
-export const PASSWORD = '';
-
-export const UPDATE_INTERVAL = 500;
-export const VEHICLE_TYPE = 'UAS';
